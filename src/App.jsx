4d8e1f60bcc13b8b00f0f3be94a53b958976ca2c
Breanna Fraire <bfraire@ucsd.edu>
2025-07-21 17:23:37 -0700

    Added motion effects

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,68 @@ import './index.css'; // Ensure this CSS file is imported
 import { motion } from "motion/react";
 
 function App() {
+  const [expanded, setExpanded] = React.useState(false);
+  const [expanded2, setExpanded2] = React.useState(false);
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-between bg-gray-100">
-      {/* Additional Section */}
       <section className="bg-grey-200 w-full py-40 mt-16">
         <div className="max-w-6xl mx-auto text-center px-6">
-
           <div className="grid grid-cols-1 md:grid-cols-3 grid-rows-4 gap-8">
 
-            <div className="p-12 bg-[url('/1.svg')] rounded-2xl shadow-md">
-              
-            </div>
+            {/* Div Block #1 - Change State*/}
+            <motion.div 
+              className={`cursor-pointer rounded-2xl shadow-md overflow-hidden bg-cover ${expanded ? 'md:row-span-3' : 'md:row-span-1'} ${expanded ? "bg-[url('/1.1.svg')]" : "bg-[url('/1.svg')]"}`}
+              onClick={() => setExpanded(!expanded)} 
+              layout
+              transition={{ duration: 0.2, ease: "easeInOut" }}
+            >
+            </motion.div>
 
+            
+            
+            {/* Div Block #2 - Static */}
             <div className="p-12 bg-[url('/2.svg')] bg-cover rounded-2xl shadow-md">
               
             </div>
-
-            <div className="p-6 bg-[url('3.svg')] bg-cover rounded-2xl shadow-md md:row-span-3">
+            
+            {/* Div Block #3 - Change State*/}
+            <motion.div
+              className={`cursor-pointer rounded-2xl shadow-md overflow-hidden bg-cover ${expanded2 ? 'md:row-span-3' : 'md:row-span-1'} ${expanded2 ? "bg-[url('/3.svg')]" : "bg-[url('/3.1.png')]"}`}
+              onClick={() => setExpanded2(!expanded2)} 
+              layout
+              transition={{ duration: 0.2, ease: "easeInOut" }}
+            >
               
-            </div>
+            </motion.div>
 
-            <div className="p-6 bg-[url('/Frame.png')] bg-cover rounded-2xl shadow-md md:row-span-3">
-            </div>
+            {/* Div Block #4 - Change State */}
+            <motion.div
+              className={`p-6 bg-[url('/Frame.png')] bg-cover rounded-2xl shadow-md ${expanded ? 'md:row-span-1 md:col-start-1 md:row-start-4' : 'md:row-span-3'} ${expanded ? "bg-[url('/4.1.svg')]" : "bg-[url('/Frame.png')]"}`}
+              onClick={() => setExpanded(!expanded)} 
+              layout
+              transition={{ duration: 0.2, ease: "easeInOut" }}
+            >
+            </motion.div>
 
+            {/* Div Block #5 - Static */}
             <div className="p-6 bg-[url('5.svg')] bg-cover  rounded-2xl shadow-md md:row-span-2">
               
             </div>
 
-            <div className="p-6 bg-[url('6.svg')] bg-cover rounded-2xl shadow-md">
+            {/* Div Block #6 - Static */}
+            <div className="p-6 bg-[url('6.svg')] bg-cover rounded-2xl shadow-md md:row-start-4 md:col-start-2">
               
             </div>
 
-            <div className="p-6 bg-[url('7.svg')] bg-cover rounded-2xl shadow-md">
-              
-            </div>
+            {/* Div Block #7 - Change State */}
+            <motion.div 
+              className={`p-6 bg-[url('/Frame.png')] bg-cover rounded-2xl shadow-md ${expanded2 ? 'md:row-span-1 md:col-start-3 md:row-start-4' : 'md:row-span-3'} ${expanded2 ? "bg-[url('/7.svg')]" : "bg-[url('/7.1.svg')]"}`}
+              onClick={() => setExpanded2(!expanded2)} 
+              layout
+              transition={{ duration: 0.2, ease: "easeInOut" }}
+            >     
+            </motion.div>
 
 
           </div>
